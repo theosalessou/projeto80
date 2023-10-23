277aceddee81c19ca7712efa48abab2f7f7eed88
Théo <77073210+theosalessou@users.noreply.github.com>
2023-10-23 19:36:16 -0300

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -18,9 +18,9 @@ export default class BottomTabNavigator extends Component {
             tabBarIcon: ({ focused, color, size }) => {
               let iconName;
 //Preencha com o código que falta para adicionar o "route name"
-            if (route.name === "bicycle") {
+            if (route.name === "Alugue uma bicicleta") {
                 iconName = "bicycle";
-              } else if (route.name === "time") {
+              } else if (route.name === "Histórico de Locações") {
                 iconName = "time";
               }  
 
